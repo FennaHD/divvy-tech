4a5d3e4ab550e5ae5aecd40991c04339d87d1d5f
Fernando Maluenda <>
2023-01-10 11:23:40 -0700

    Reverse revenue lists so that oldest dates are first

--- app/src/main/java/com/divvy/domain/Business.kt
+++ app/src/main/java/com/divvy/domain/Business.kt
@@ -1,18 +1,23 @@
 package com.divvy.domain
 
+import androidx.lifecycle.MutableLiveData
 import com.jaikeerthick.composable_graphs.data.GraphData
 
 data class Business(
     val id: Int?,
     val name: String?,
     val location: BusinessLocation?,
+    // Note that this revenue will be ordered from closest to furthest date
     val revenue: List<RevenueEntry>?
 ) {
-    fun getXAxis() = revenue?.mapNotNull {
+    // Total revenue for the range the user selects in the RangeSlider
+    val totalRevenue = MutableLiveData<Double>(null)
+
+    fun getXAxis() = revenue?.reversed()?.mapNotNull {
         it.getDateLabel()?.let {
             GraphData.String(it)
         }
     }.orEmpty()
 
-    fun getYAxis() = revenue?.mapNotNull { it.value }.orEmpty()
+    fun getYAxis() = revenue?.reversed()?.mapNotNull { it.value }.orEmpty()
 }
